2115ba90a86fd8b3fc62dda9a331500e08669b56
Jacob Titong <titongjacob@gmail.com>
2026-08-03 21:13:22 +0800

    feat: create addLog feature for back-end

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function LogLibrary(library) {
+  this.library = library;
+}
+
+LogLibrary.prototype.addLog = function (title, description, date, author) {
+  this.library.push(new Log(title, description, date, author));
+};
+
+function Log(title, description, date, author) {
+  this.title = title;
+  this.description = description;
+  this.date = date;
+  this.author = author;
+  this.id = crypto.randomUUID();
+}
+
+// Displaying and Hiding Dialog
 const dialogLog = document.querySelector(".dialog-log");
 const showButton = document.querySelector(".add-log button");
 const closeButton = document.querySelector(".dialog-log .exit-button");
@@ -13,4 +30,24 @@ closeButton.addEventListener("click", () => {
 document.addEventListener("click", (e) => {
   if (!e.target.contains(dialogLog)) return;
   dialogLog.close();
+});
+
+// Initialize library of logs
+const logLibrary = new LogLibrary([]);
+
+// Gathering form data
+let formDataObj;
+const form = document.querySelector(".dialog-log form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  formDataObj = Object.fromEntries(formData);
+  dialogLog.close();
+
+  logLibrary.addLog(
+    formDataObj.heading,
+    formDataObj.paragraph,
+    formDataObj.date,
+    formDataObj.author,
+  );
 });
